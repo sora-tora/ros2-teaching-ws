434c2075c01ac4c037da632a90cc3cd24a052a46
Lincoln Computing Student <student@socstech.support>
2024-07-26 18:40:39 +0000

    Final Fixes

--- py_pubsub/py_pubsub/colour_chaser.py
+++ py_pubsub/py_pubsub/colour_chaser.py
@@ -13,7 +13,10 @@ from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Image
 import cv2
 import numpy as np
 from rclpy.executors import MultiThreadedExecutor
-
+global pushOnwards
+pushOnwards = False
+global stuck
+stuck = 0
 
 class ColourChaser(Node):
     def __init__(self):
@@ -30,6 +33,8 @@ class ColourChaser(Node):
 
     def camera_callback(self, data):
         #self.get_logger().info("camera_callback")
+        global pushOnwards
+        global stuck
 
         cv2.namedWindow("Image window", 1)
 
@@ -45,54 +50,73 @@ class ColourChaser(Node):
         contours, hierarchy = cv2.findContours(current_frame_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         # Sort by area (keep only the biggest one)
-        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:1]
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)#[:1]
 
         # Draw contour(s) (image to draw on, contours, contour number -1 to draw all contours, colour, thickness):
         current_frame_contours = cv2.drawContours(current_frame, contours, 0, (255, 255, 0), 20)
 
         self.tw=Twist() # twist message to publish
         if len(contours) > 0:
+            for i in contours:
             # find the centre of the contour: https://docs.opencv.org/3.4/d8/d23/classcv_1_1Moments.html
-            M = cv2.moments(contours[0]) # only select the largest contour
-            if M['m00'] > 0:
-                # find the centroid of the contour
-                cx = int(M['m10']/M['m00'])
-                cy = int(M['m01']/M['m00'])
-                print("Centroid of the biggest area: ({}, {})".format(cx, cy))
+                M = cv2.moments(i) # only select the largest contour
+                if M['m00'] > 0:
+                    # find the centroid of the contour
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+                    if cy <= data.height/2:
+                        if len(contours) == 1:
+                            self.tw.angular.z = 0.6#wallspin
+                        continue
+                    if stuck == cy:
+                        #self.tw.linear.x=-100.0
+                        self.tw.angular.z=3.0
+                        print("I'm stuck")
+                        break
+                    stuck = cy
+                    print("Centroid of the biggest area: ({}, {})".format(cx, cy))
 
-                # Draw a circle centered at centroid coordinates
-                # cv2.circle(image, center_coordinates, radius, color, thickness) -1 px will fill the circle
-                cv2.circle(current_frame, (round(cx), round(cy)), 5, (0, 255, 0), -1)
-                            
-                # find height/width of robot camera image from ros2 topic echo /camera/image_raw height: 1080 width: 1920
+                    # Draw a circle centered at centroid coordinates
+                    # cv2.circle(image, center_coordinates, radius, color, thickness) -1 px will fill the circle
+                    cv2.circle(current_frame, (round(cx), round(cy)), 5, (0, 255, 0), -1)
+                                
+                    # find height/width of robot camera image from ros2 topic echo /camera/image_raw height: 1080 width: 1920
 
-                # if center of object is to the left of image center move left
-                if cx < data.width / 5:
-                    self.tw.angular.z=0.4
-                    self.tw.linear.x=100.0
-                # else if center of object is to the right of image center move right
-                elif cx >= 2 * data.width / 5:
-                    self.tw.angular.z=-0.4
-                    self.tw.linear.x=100.0
-                else: # center of object is in a 100 px range in the center of the image so dont turn
-                    print("object in the center of image")
-                    self.tw.angular.z=0.5
-                    #self.tw.linear.x=1000000000000000000.0*100000000000
-                    #self.tw.linear.x=100.0
-
+                    # if center of object is to the left of image center move left
+                    if pushOnwards:
+                        print("push!")
+                        self.tw.linear.x=25000000000.0
+                        pushOnwards = False
+                    elif (cx < data.width / 3):
+                        print("Turning Left")
+                        self.tw.angular.z=0.6
+                        self.tw.linear.x=-0.05
+                    # else if center of object is to the right of image center move right
+                    elif (cx >= 2 * data.width / 3):
+                        print("Turning Right")
+                        self.tw.angular.z=-0.6
+                        self.tw.linear.x=-0.05
+                    else: # center of object is in a 100 px range in the center of the image so dont turn
+                        print("object in the center of image")
+                        self.tw.linear.x=25000000000.0
+                        pushOnwards = True
+                    break
+                
         else:
             print("No Centroid Found")
             # turn until we can see a coloured object
-            self.tw.angular.z=2.0##Ooh, put the big push here!
+            self.tw.angular.z=3.0##Ooh, put the big push here!
             #self.tw.linear.x=100.0
 
         self.pub_cmd_vel.publish(self.tw)
-
 
         # show the cv images
         current_frame_contours_small = cv2.resize(current_frame_contours, (0,0), fx=0.4, fy=0.4) # reduce image size
         cv2.imshow("Image window", current_frame_contours_small)
         cv2.waitKey(1)
+    def wall_rebound(self, data):
+        if (400 <= cx <= 420) and (460 <= cy <= 480):
+            print('Haha')
 
 def main(args=None):
     print('Starting colour_chaser.py.')
