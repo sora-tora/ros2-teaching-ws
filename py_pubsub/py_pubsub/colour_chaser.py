713cbe0dcbb2f1e9a262a491e9d2b122f5901599
Lincoln Computing Student <student@socstech.support>
2024-07-26 13:56:38 +0000

    Slight Changes

--- py_pubsub/py_pubsub/colour_chaser.py
+++ py_pubsub/py_pubsub/colour_chaser.py
@@ -51,10 +51,9 @@ class ColourChaser(Node):
         current_frame_contours = cv2.drawContours(current_frame, contours, 0, (255, 255, 0), 20)
 
         self.tw=Twist() # twist message to publish
-        
         if len(contours) > 0:
             # find the centre of the contour: https://docs.opencv.org/3.4/d8/d23/classcv_1_1Moments.html
-            M = cv2.moments(contours[0]) # only select the largest controur
+            M = cv2.moments(contours[0]) # only select the largest contour
             if M['m00'] > 0:
                 # find the centroid of the contour
                 cx = int(M['m10']/M['m00'])
@@ -68,22 +67,27 @@ class ColourChaser(Node):
                 # find height/width of robot camera image from ros2 topic echo /camera/image_raw height: 1080 width: 1920
 
                 # if center of object is to the left of image center move left
-                if cx < data.width / 3:
-                    self.tw.angular.z=0.3
+                if cx < data.width / 5:
+                    self.tw.angular.z=0.4
+                    self.tw.linear.x=100.0
                 # else if center of object is to the right of image center move right
-                elif cx >= 2 * data.width / 3:
-                    self.tw.angular.z=-0.3
+                elif cx >= 2 * data.width / 5:
+                    self.tw.angular.z=-0.4
+                    self.tw.linear.x=100.0
                 else: # center of object is in a 100 px range in the center of the image so dont turn
                     print("object in the center of image")
-                    self.tw.angular.z=0.0
-                    self.tw.linear.x=0.1
-                    
+                    self.tw.angular.z=0.5
+                    #self.tw.linear.x=1000000000000000000.0*100000000000
+                    #self.tw.linear.x=100.0
+
         else:
             print("No Centroid Found")
             # turn until we can see a coloured object
-            self.tw.angular.z=0.3
+            self.tw.angular.z=2.0##Ooh, put the big push here!
+            #self.tw.linear.x=100.0
 
         self.pub_cmd_vel.publish(self.tw)
+
 
         # show the cv images
         current_frame_contours_small = cv2.resize(current_frame_contours, (0,0), fx=0.4, fy=0.4) # reduce image size
